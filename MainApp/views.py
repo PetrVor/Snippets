e2195ca8d7f3fa8af1cf6071d921e7ad5300bc94
PetrVor <ninja-xiii@mail.ru>
2025-03-15 21:44:29 +0300

    Complete Modeulw 5 task 13 + additional errors

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,7 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -19,3 +20,13 @@ def snippets_page(request):
             'pagename': 'Просмотр сниппетов',
             'snippets': snippets}
     return render(request, 'pages/view_snippets.html', context)
+
+def snippet_detail(request, snippet_id):
+    context ={'pagename': 'просмотр сниппета'}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:    
+        return render(request,"pages/error.html", context | {"error" : f"Snippet with id:{snippet_id} not found!"}) 
+    else:      
+        context['snippet'] = snippet
+    return render(request,'pages/snippet_page.html', context)
