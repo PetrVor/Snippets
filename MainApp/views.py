8945faf5881ab8fac47f08fe5f82db7ae6548fce
PetrVor <ninja-xiii@mail.ru>
2025-03-18 21:00:54 +0300

    Add login and logout

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from MainApp.forms import SnippetForm
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import auth
 
 
 def index_page(request):
@@ -76,6 +77,26 @@ def snippet_delete(request, snippet_id):
         snippet = get_object_or_404(Snippet, id=snippet_id)
         snippet.delete()
     return redirect("snippets-list")
+
+
+def login(request):
+   if request.method == 'POST':
+       username = request.POST.get("username")
+       password = request.POST.get("password")
+       # print("username =", username)
+       # print("password =", password)
+       user = auth.authenticate(request, username=username, password=password)
+       if user is not None:
+           auth.login(request, user)
+       else:
+           # Return error message
+           pass
+   return redirect('home')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("home")
         
 
 
